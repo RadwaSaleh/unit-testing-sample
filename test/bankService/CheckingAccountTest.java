2724a61dd4ced542755ce5f975d6a612a46c3707
Radwa Saleh <radwa.saleh@pixelogicmedia.com>
2020-04-01 00:20:52 +0200

    Using Spy on email service

--- test/bankService/CheckingAccountTest.java
+++ test/bankService/CheckingAccountTest.java
@@ -1,11 +1,16 @@
 package bankService;
 
+import notificationService.EmailService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.mockito.Matchers.contains;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
@@ -45,5 +50,32 @@ public class CheckingAccountTest {
 
         //Then
         assertEquals(checkingAccount.getBalance(), 150);
+    }
+
+    @Spy
+    EmailService spiedEmailService = new EmailService();
+
+    /**
+     * Customers should be able to receive notifications that a check has been processed
+     * Scenario:
+     * 1. Given I enable E-mail notifications on the checking account
+     * 2. And I have a valid check #4321 for amount $100.00
+     * 3. When I process check #4321
+     * 4. Then the transaction notification should be sent ia e-mail
+     * 5. And the account balance should be $150.00
+     */
+    @Test
+    public void processingCheckWithNotificationsEnabled_DecreaseBalanceAndSendMessage(){
+        //Given
+        checkingAccount.enableNotifications(spiedEmailService);
+        when(mockCheck.getAmount()).thenReturn(100.00);
+        when(mockCheck.getCheckNumber()).thenReturn(4321);
+
+        //When
+        checkingAccount.processCheck(mockCheck);
+
+        //Then
+        verify(spiedEmailService).notify(contains("Check #4321"), eq(checkingAccount.getOwner().getEmail()));
+        assertEquals(checkingAccount.getBalance(), 200);
     }
 }
